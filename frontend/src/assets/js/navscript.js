123922e5adf373be6b2f3aba60a8fe139384b8c5
Abhilash Raju <abhilash.raju8@gmail.com>
2022-08-11 21:53:09 +0530

    Responsive

--- frontend/src/assets/js/navscript.js
+++ frontend/src/assets/js/navscript.js
@@ -21,8 +21,4 @@ window.addEventListener('DOMContentLoaded', () => {
         }
         scrollPos = currentTop;
     });
-
-    window.addEventListener('scroll', () => {
-        document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-      }, false);
 })
